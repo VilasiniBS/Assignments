d421d8cd89cd4933e337af03fe1511a83de763df
VilasiniBS <vilasinibs30@gmail.com>
2025-04-08 19:48:45 +0530

    Assignments

---
+++ Automation_Project/src/langiage/basis/DebugProg.java
@@ -0,0 +1,18 @@
+package langiage.basis;
+
+
+
+public class DebugProg {
+
+	public static void main(String[] args) {
+		System.out.println("Step 1");
+		System.out.println("Step 1");
+		System.out.println("Step 1");
+		System.out.println("Step 1");
+		System.out.println("Step 1");
+		System.out.println("Step 1");
+		
+
+	}
+
+}
