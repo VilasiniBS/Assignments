6b404c27330374b58c76f437e89aee5b58531cdb
VilasiniBS <vilasinibs30@gmail.com>
2025-04-09 01:11:34 +0530

    Datatypes assignments

--- Assignments/src/assignments/DataTypes.java
+++ Assignments/src/assignments/DataTypes.java
@@ -3,8 +3,94 @@ package assignments;
 public class DataTypes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Temperature of a city in degrees celsis: 25.5 (datatype =float)
+		float temp = 25.5f;
+		System.out.println("tempature of city in degree celsis : " + temp);
+		
+		//Whether a customer has placed an order: true (datatype= boolean)
+		boolean customerorder = true;
+		System.out.println("Customer order is placed" + customerorder);
+		
+		//Person 's phone number "123-456-7890" (datatype= String)
+		String PhoneNumber = "123-456-7890";
+	    System.out.println("Person phonenumber is :" + PhoneNumber);
+	    
+	    //Amount of money in a customer's bank account 1000.50 (datatype= float)
+	    float AccountNo = 1000.50f;
+		System.out.println("Amount of customer bank account is : " + AccountNo );
+		
+		//Person 's email address : "john.doe@example.com"  (datatype=String)
+		String emailid = "john.doe@example.com";
+		System.out.println("person email id is : " + emailid);
+		
+		//Coordinates of a location (latitude, longitude): 37.7749, -122.4194 (datatype = Array)
+		float[] Loc = { 37.77749f,  -122.4194f };
+		System.out.println("Cordinates of location latitude :  " + Loc[0]  +  " and langitude : " + Loc[1]);
+		
+		//Person's marital status : true or false (datatype = boolean)
+		boolean Status = false;
+		System.out.println("Person's marital status is" + Status);
+		
+		//Person's Occupation : "Software Engineer"( datatype = String)
+		String Occup = "Software Engineer";
+		System.out.println("person's Occupation is : " + Occup);
+		
+		//Person's fav color "Blue" (datatype = String)	
+		String col = "Blue";
+		System.out.println("person's fav color : " + col);
+		
+		//Current year 2025 (datatype = short)
+		short year = 2025;
+		System.out.println("Current year is : " + year);
+		
+		//Number of followers in social media platform :1000000 (datatype = int)
+		int followers = 1000000;
+		System.out.println("No of folloers in social media is : " + followers);
+		
+		//Rating of a movie: 7.5 (datatype= float)
+		float movie = 7.5f;
+		System.out.println("Rating of movie is :" + movie);
+		
+		//Person 's blood type :A (datatype = char)
+		char bloodType ='A';
+		System.out.println(" person's blood type is : "+ bloodType);
+		
+		//Title of a book "To kill a mocklingbird" (datatype = String)
+		String title = "To kill a MockingBird";
+		System.out.println("Title of the book is : "+ title);
+		
+		//No of employees in a company 500 (dataype: short)
+		short CmpEmp = 500;
+		System.out.println("No of employees in a company is : " + CmpEmp);
+		
+		//Time of an event : 2.30 PM ( datatype = String)
+		String Time = "2.30 PM";
+		System.out.println("Time of an event  : " + Time);
+		
+		//Name of the country "United states" (datatype = String)
+		String Country = "United State";
+		System.out.println("Name of the country : " + Country);
+		
+		//Person's eyecolor "Brown"( datatype= String)
+		String Eyecolor = "Brown";
+		System.out.println("Person's Eyecolor is : " + Eyecolor);
+		
+		//Person's Birthplace "New York City"( datatype= String)
+		String Birthplace = "New York City";
+		System.out.println("Person's Birthplace is : " + Birthplace);
+		
+		//Distance between two cities:200.5( datatype= float)
+	    float distance = 200.5f;
+		System.out.println("Distance between two cities is : " + distance);
+		
+		
+		
 	}
-
 }
+
+
+				
+
+		
+
+		
